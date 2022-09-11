0c4b697677d9ed11226b64f314acfe09be61b8f5
ZHOU <510878689@qq.com>
2022-09-11 14:44:14 +0800

    feat：新增文章模糊搜索、日期筛选、文章形式、是否置顶功、解析文章所有图片能

--- includes/api/m_article.php
+++ includes/api/m_article.php
@@ -27,9 +27,9 @@ class M_Article extends WP_REST_Controller{
         $query_params = $request->get_query_params();
         $page = (int)$query_params['page'] ?: 1;
         $posts_per_page = (int)$query_params['size'] ?: 10;
-        $post_type = $query_params['postType'] ?: 'post'; // post, page
-        $post_status = $query_params['postStatus'] ?: 'publish'; // publish, future, draft, pending, private, trash
-        $orderby = $query_params['orderby'] ?: 'date'; // ID, date, modified, rand, comment_count
+        $post_type = $query_params['type'] ?: 'post'; // post, page
+        $post_status = $query_params['status'] ?: 'publish'; // publish, future, draft, pending, private, trash
+        $orderby = $query_params['orderby'] ?: 'date'; // ID, date, title, modified, rand, comment_count
         $order = $query_params['order'] ?: 'DESC'; // DESC, ASC
 
         $args = array(
@@ -40,17 +40,64 @@ class M_Article extends WP_REST_Controller{
             'orderby'           =>  $orderby,
             'order'             =>  $order
         );
-        if (!empty($query_params['title'])) { // 文章标题，可用来做搜索
-            $args['title'] = $query_params['title'];
+
+        // $title = $query_params['title'];
+        // if (!empty($title)) { // 文章标题，需完全匹配，似乎没多大用
+        //     $args['title'] = $title;
+        // }
+        $search = $query_params['search'];
+        if (!empty($search)) { // 文章标题模糊搜索
+            $args['s'] = $search;
         }
-        if (!empty($query_params['authorId'])) { // 文章作者
-            $args['author'] = $query_params['authorId'];
+        $authorId = $query_params['authorId'];
+        if (!empty($authorId)) { // 文章作者id，多个用英文逗号分隔
+            $args['author'] = $authorId;
         }
-        if (!empty($query_params['cateId'])) { // 分类id
-            $args['cat'] = (int)$query_params['cateId'];
+        $cateId = $query_params['cateId'];
+        if (!empty($cateId)) { // 分类id，多个用英文逗号分隔
+            $args['cat'] = (int)$cateId;
         }
-        if (!empty($query_params['tagId'])) { // 标签id
-            $args['tag_id'] = (int)$query_params['tagId'];
+        $tagId = $query_params['tagId'];
+        if (!empty($tagId)) { // 标签id，多个用英文逗号分隔
+            $args['tag_id'] = $tagId;
+        }
+        $sticky = $query_params['sticky'];
+        if (!empty($sticky) && $sticky === 'true') { // 是否只查询置顶文章
+            // 注意参数中间是两个下划线
+            $args['post__in'] = get_option( 'sticky_posts' );
+        }
+
+        // https://developer.wordpress.org/reference/functions/get_posts/
+        // 时间日期
+        // year int Accepts any four-digit year
+        // monthnum int 1-12
+        // day int 1-31
+        // hour int 0-23
+        // minute int 0-59
+        // second int 0-59
+        $year = $query_params['year'];
+        if (!empty($year)) { // 4个数字年份
+            $args['year'] = (int)$year;
+        }
+        $monthnum = $query_params['monthnum'];
+        if (!empty($monthnum)) { // 月份
+            $args['monthnum'] = (int)$monthnum;
+        }
+        $day = $query_params['day'];
+        if (!empty($day)) { // 天
+            $args['day'] = (int)$day;
+        }
+        $hour = $query_params['hour'];
+        if (!empty($hour)) { // 小时
+            $args['hour'] = (int)$hour;
+        }
+        $minute = $query_params['minute'];
+        if (!empty($minute)) { // 分钟
+            $args['minute'] = (int)$minute;
+        }
+        $second = $query_params['second'];
+        if (!empty($second)) { // 秒
+            $args['second'] = (int)$second;
         }
 
         $query = new WP_Query( $args );
@@ -64,12 +111,19 @@ class M_Article extends WP_REST_Controller{
 
             // 注意：类自身的方法要通过 $this 去调用
             $img = $this->get_article_img($id, $content);
+            $imgs = $this->get_html_images($content);
+
             $list[] = array(
                 "id" => $id,
                 "title" => $post->post_title,
                 "postDate" => $post->post_date,
                 "commentCount" => (int)$post->comment_count,
-                "img" => $img,
+                "img" => $img, // 文章设置的特色图
+                "imgs" => $imgs, // 文章内容中解析出的所有图片地址
+                "format" => get_post_format($id) ?: 'standard', // WP_Query里查出来无这个字段，注意默认的标准形式 get_post_format 也会返回 false
+                "sticky" => is_sticky($id), // 是否置顶
+                "status" => $post->post_status, // 文章状态
+                "type" => $post->post_type, // 文章类型
             );
         }
         $data["list"] = $list;
@@ -95,10 +149,7 @@ class M_Article extends WP_REST_Controller{
         $postId = $post->ID;
 
         if (empty($post) || empty($postId)) {
-            // $error["code"] = "20001";
-            // $error["message"] = "文章不存在";
-            // $error["data"] = "";
-            return new WP_Error("20001", "文章不存在", "");
+            return new WP_Error(20001, "文章不存在", "");
         }
         $authorId = $post->post_author;
         $user = get_user_by('id', $authorId);
@@ -115,6 +166,11 @@ class M_Article extends WP_REST_Controller{
         $data["authorId"] = $authorId;
         $data["author"] = $author_name;
         $data["commentCount"] = (int)$post->comment_count;
+        $data["commentStatus"] = $post->comment_status; // 是否可以评论 open, closed
+        $data["format"] = get_post_format($postId) ?: 'standard'; // 文章格式(WP_Query里查出来无下面两个字段)
+        $data["sticky"] = is_sticky($postId); // 是否置顶
+        $data["status"] = $post->post_status; // 文章状态
+        $data["type"] = $post->post_type; // 文章类型
         $data["categories"] = $categories;
         $data["tags"] = $tags;
 
@@ -137,6 +193,30 @@ class M_Article extends WP_REST_Controller{
         } else {
             return '';
         }
+    }
+
+    /*
+    * 解析HTML中的所有图片地址
+    * $content 参数
+    * $index 可选参数，指定返回第几个，默认 all 返回所有
+    * return：如果指定了 $index 值 返回 字符串 否则 返回数组参数
+    * get_html_images($content,0) 返回第一个图片地址
+    * get_html_images($content) 返回数组
+    * $pattern = "/<img.*?src=[\'|\"](.*?(?:[\.gif|\.GIF|\.jpg|\.JPG|\.jpge|\.JPGE|\.png|\.PNG]))[\'|\"].*?[\/]?>/";
+    */
+    private function get_html_images($content, $index = 'all'){
+        $pattern = "<img.*?src=[\'｜\"](.*?)[\'|\"].*?>";
+        preg_match_all($pattern, $content, $match);
+
+        if(isset($match[1]) && !empty($match[1])){
+            if($index==='all'){
+                return $match[1];
+            }
+            if(is_numeric($index) && isset($match[1][$index])){
+                return $match[1][$index];
+            }
+        }
+        return null;
     }
 
     // 格式化数据
