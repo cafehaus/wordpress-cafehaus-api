7ac4d37d73084c4255c349fd6ded2438cbe835da
ZHOU <510878689@qq.com>
2022-09-11 23:30:55 +0800

    feat：修改插件信息

--- wordpress-cafe-api.php
+++ wordpress-cafe-api.php
@@ -1,9 +1,11 @@
 <?php
 /*
-Plugin Name: Wordpress Cafe Api
-Description: 支持多端前后端分离开发的 REST API 接口
-Version: 0.0.1
+Plugin Name: CAFE API
+Description: 兼容小程序、APP和H5的多端 API 插件，提供更加优雅的路由、入参和出参，开箱即用零依赖零设置，让前端用着更省心
+Version: 1.0.0
 Author: cafehaus
+Author URI: https://github.com/cafehaus
+License: GPL v3
 */
 
 define('CafeApiPlugin_DIR', plugin_dir_path(__FILE__));
